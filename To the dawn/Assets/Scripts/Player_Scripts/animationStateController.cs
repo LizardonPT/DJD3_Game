16adb05ea160f484bbacb1d76ab2469fb09101e4
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2021-04-23 17:02:55 +0100

    Add basic idle and running animation

---
+++ To the dawn/Assets/Scripts/Player_Scripts/animationStateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if (animator.GetBool("isRunning") && !Input.GetButton("Horizontal") && !Input.GetButton("Vertical"))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
