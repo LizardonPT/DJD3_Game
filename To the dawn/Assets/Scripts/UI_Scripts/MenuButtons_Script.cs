ab5ae54abd514903f424dfddefd3b475199a67eb
António Branco <ajsbranco15@gmail.com>
2021-04-23 17:12:34 +0100

    Add Basic Menu

---
+++ To the dawn/Assets/Scripts/UI_Scripts/MenuButtons_Script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class which interact with the buttons pressed by the player in
+/// the main menu.
+/// </summary>
+public class MenuButtons_Script : MonoBehaviour
+{
+    /// <summary>
+    /// Public method that loads the next scene in the build settings order.
+    /// </summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Public method that leaves the application, does not work on the editor.
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
