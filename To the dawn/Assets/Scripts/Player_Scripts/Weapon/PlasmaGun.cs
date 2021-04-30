6983987f7707ad11b18e0cc0947bde5d47bf268a
António Branco <ajsbranco15@gmail.com>
2021-04-30 20:18:44 +0100

    Add plasma visual

--- To the dawn/Assets/Scripts/Player_Scripts/Weapon/PlasmaGun.cs
+++ To the dawn/Assets/Scripts/Player_Scripts/Weapon/PlasmaGun.cs
@@ -9,8 +9,14 @@ public class PlasmaGun : MonoBehaviour
     [SerializeField] private int damage = 3;
     [SerializeField] private int useEnergyPerShoot = 2;
     [SerializeField] private Transform firePoint;
+    [SerializeField] private LineRenderer lineRend;
 
     private float timer;
+
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,17 +33,25 @@ public class PlasmaGun : MonoBehaviour
     {
         Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
         Debug.DrawRay(firePoint.position, ray.direction * 100, Color.blue, 2f);
+        Vector3 direction = (ray.direction * 100 - firePoint.position).normalized;
         RaycastHit hitInfo;
 
+        LineRenderer plasmalr = Instantiate(lineRend, firePoint.position, Quaternion.LookRotation(direction));
+        //Draw line
+        //LineRenderer plasmalr = plasma.GetComponent<LineRenderer>();
         if (Physics.Raycast(ray,out hitInfo, 100))
         {
+            plasmalr.SetPosition(1, new Vector3(0,0,hitInfo.distance));
             HP hp = hitInfo.collider.gameObject.GetComponent<HP>();
             if(hp)
             {
                 hp.HPModifier(damage, "plasma");
             }
         }
-
+        else{
+            plasmalr.SetPosition(1, new Vector3(0,0,500));
+        }
+        Destroy(plasmalr.gameObject, 0.3f);
         gameObject.GetComponent<Energy>().UpdateEnergy(useEnergyPerShoot);
     }
 }
