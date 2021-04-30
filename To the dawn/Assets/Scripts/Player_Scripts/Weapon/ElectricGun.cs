4dae9125f45a4ed41e3a353460e00f09e18b53ba
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2021-04-30 17:11:33 +0100

    Add lightning to electric gun

--- To the dawn/Assets/Scripts/Player_Scripts/Weapon/ElectricGun.cs
+++ To the dawn/Assets/Scripts/Player_Scripts/Weapon/ElectricGun.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DigitalRuby.LightningBolt;
 
 public class ElectricGun : MonoBehaviour
 {
     [SerializeField] private float fireRate = 0.5f;
-
+    [SerializeField] private GameObject thunder;
     [SerializeField] private int damage = 2;
     [SerializeField] private int useEnergyPerShoot = 1;
-
     [SerializeField] private Transform firePoint;
 
     private float timer;
@@ -26,19 +26,39 @@ public class ElectricGun : MonoBehaviour
 
     private void FireGun()
     {
+        // Create ray from the camera, with the directiom from the gun to 
+        // the end of the ray
         Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
-        Debug.DrawRay(firePoint.position, ray.direction * 100, Color.yellow, 2f);
+        Vector3 direction = (ray.direction * 100 - firePoint.position).normalized;
         RaycastHit hitInfo;
 
+        // Creates thunder effect
+        GameObject newthunder = Instantiate(thunder, firePoint.position, Quaternion.LookRotation(direction));
+        newthunder.GetComponent<LightningBoltScript>().StartPosition = firePoint.position;
+
+        // If it hits something ...
         if (Physics.Raycast(ray,out hitInfo, 100))
         {
+            // ... creates thunder effect until impact and removes HP
+            newthunder.GetComponent<LightningBoltScript>().EndPosition = hitInfo.point;
+
             HP hp = hitInfo.collider.gameObject.GetComponent<HP>();
             if(hp)
             {
                 hp.HPModifier(damage, "electric");
             }
         }
+        // If it does not ...
+        else
+        {
+            // ... just creates a thunder effect to the end of the ray
+            newthunder.GetComponent<LightningBoltScript>().EndPosition = ray.direction * 100;
+        }
 
+        // Removes thunder from the game
+        Destroy(newthunder, 0.1f);
+
+        // Removes Energy
         gameObject.GetComponent<Energy>().UpdateEnergy(useEnergyPerShoot);
     }
 }
