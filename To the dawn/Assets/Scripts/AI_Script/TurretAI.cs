48c0c8672479374d9217f315e0f026b40fd39156
António Branco <ajsbranco15@gmail.com>
2021-05-03 14:51:54 +0100

    Add turret AI

---
+++ To the dawn/Assets/Scripts/AI_Script/TurretAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    [SerializeField] private LayerMask whatIsPlayer= default;
+    [SerializeField] private float sightRange;
+    [SerializeField] private float cooldown;
+    private bool alreadyAttacked;
+
+    private bool playerInSightRange;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if(playerInSightRange) TurretAiAttack();
+    }
+
+    private void TurretAiAttack()
+    {
+        if(!alreadyAttacked)
+        {
+            Debug.Log("A turret is attacking you!!");
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), cooldown);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}
