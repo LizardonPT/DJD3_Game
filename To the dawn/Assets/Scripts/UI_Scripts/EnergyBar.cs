203c0bd79650a1919b28b568587db783a541a8b5
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2021-06-17 22:22:26 +0100

    Energy bar WIP (upgrading UI)

---
+++ To the dawn/Assets/Scripts/UI_Scripts/EnergyBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class EnergyBar : MonoBehaviour
+{
+    [SerializeField] private int maximum;
+    [SerializeField] private int current;
+    [SerializeField] private Image mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+}
